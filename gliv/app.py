072950775899553b2a42ab8a000ee6f310200395
Michael Osthege <michael.osthege@outlook.com>
2024-12-29 14:56:50 +0100

    Use sidebar layout

--- gliv/app.py
+++ gliv/app.py
@@ -29,7 +29,7 @@ def embed_svg(svg: str, zoom: float=1):
 
 def run():
     st.set_page_config(layout="wide")
-    st.header("GitLab Issue Visualizer")
+    st.sidebar.header("GitLab Issue Visualizer")
 
     issues: dict[int, Issue] = pickle.load(open(DP_PICKLES / "issues_conv.p", 'rb'))
     links_related: list[Link] = pickle.load(open(DP_PICKLES / "links_related.p", 'rb'))
@@ -38,19 +38,18 @@ def run():
 
     # Graph customization settings
     pnames_pids = {pname:pid for pid,pname in get_projects().items()}
-    pnames_selected = st.multiselect(
+    pnames_selected = st.sidebar.multiselect(
         "Choose projects",
         options=sorted(pnames_pids),
         default=pnames_pids,
     )
     pids_selected = {pnames_pids[pid] for pid in pnames_selected}
-    c0, c1 = st.columns([0.3, 0.7])
-    with_closed = c0.selectbox(
+    with_closed = st.sidebar.selectbox(
         "For closed issues",
         options=["titles", "numbers", "hide"],
         index=2,
     )
-    graph_zoom = c1.slider("Zoom factor", min_value=1.0, max_value=3.0, value=1.0, step=0.1)
+    graph_zoom = st.sidebar.slider("Zoom factor", min_value=1.0, max_value=3.0, value=1.0, step=0.1)
 
     # Filter according to the user settings
     def issue_filter(i: Issue) -> bool:
@@ -59,7 +58,7 @@ def run():
         return i.project_id in pids_selected
 
     issues_selected = {i:iss for i, iss in issues.items() if issue_filter(iss)}
-    st.write(f"Selected {len(issues_selected)} issues from {len(pids_selected)} projects.")
+    st.sidebar.write(f"Selected {len(issues_selected)} issues from {len(pids_selected)} projects.")
 
     # Draw the SVG and embed it on the page
     svgpath = render.render_issues_with_links(
